6aa246d76ba02dc7886b11371544e284cbe7d8b6
nrbns357 <gusgh357@gmail.com>
2021-10-03 20:04:04 +0900

    upDate: 10월 30일

--- Questions.js
+++ Questions.js
@@ -199,3 +199,83 @@ for(let i = 0; i <= 2; i++) {
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 input.sort((a, b) => a -b);
 console.log(input.join(' '));
+
+
+
+
+
+//10월 3일 2741 문제 [N 찍기]
+//{문제}
+//자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+//코드
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line) {
+  const input = line;
+    const output = [];
+    for(let i=1; i <= input; i++){
+        output[i-1]=i;
+    }
+    console.log(output.join("\n"));
+    
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
+
+
+//다른 방법
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line) {
+  const input = Number(line);
+    let str = '';
+    for(let i=1; i <= input; i++){
+        str+=(i+'\n');
+    }
+    console.log(str.slice(0, -1));
+    
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
+
+
+
+
+
+//10월 3일 2742 문제 [기찍 N]
+//{문제}
+//자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+//코드
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(line) {
+  const input = Number(line);
+    let str = '';
+    for(let i=input; i >= 1; i--){
+        str+=(i+'\n');
+    }
+    console.log(str.slice(0, -1));
+    
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
